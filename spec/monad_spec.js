9e26392513e7df9bbfc6a181548263bb1fc119c4
Oleksii Fedorov <waterlink000@gmail.com>
2015-07-01 23:15:01 +0300

    Implement pattern matching

--- spec/monad_spec.js
+++ spec/monad_spec.js
@@ -1,6 +1,10 @@
-var curry = require("../src/monad").curry
-var functor = require("../src/monad").functor
-var Maybe = require("../src/monad").Maybe
+var m = require("../src/monad")
+var curry = m.curry
+var pattern = m.pattern
+var match = m.match
+var _ = m._
+var functor = m.functor
+var Maybe = m.Maybe
 
 context = describe
 
@@ -81,6 +85,83 @@ describe("functors", function() {
     it("works as expected", function() {
       expect(new Maybe.Just(71).fmap(mult(3))).toEqual(new Maybe.Just(213))
       expect(new Maybe.Nothing().fmap(mult(3))).toEqual(new Maybe.Nothing)
+    })
+  })
+})
+
+describe("pattern matching", function() {
+  var Cat = function() {},
+      Dog = function() {},
+      CatFood = function() {},
+      DogFood = function() {}
+
+  var cat = new Cat,
+      dog = new Dog,
+      catFood = new CatFood,
+      dogFood = new DogFood
+
+  describe("a multi-argument function", function() {
+    var eat = match(
+      pattern(Cat, CatFood, Number,
+        function(cat, food, amount) {
+          return new Maybe.Just("cat ate " + amount + " of cat food")
+        }),
+
+      pattern(Dog, DogFood, Number,
+        function(dog, food, amount) {
+          return new Maybe.Just("dog ate " + amount + " of dog food")
+        }),
+
+      pattern(_, _, _,
+        function() { return new Maybe.Nothing })
+    )
+
+    it("chooses function correctly when pattern matches", function() {
+      expect(eat(cat)(catFood, 4)).toEqual(new Maybe.Just("cat ate 4 of cat food"))
+      expect(eat(dog, dogFood)(3)).toEqual(new Maybe.Just("dog ate 3 of dog food"))
+    })
+
+    it("works correctly with 'all-catching' _ patterns", function() {
+      expect(eat(cat, catFood, "hello world")).toEqual(new Maybe.Nothing)
+      expect(eat(cat, dog, 7)).toEqual(new Maybe.Nothing)
+      expect(eat(cat, dog, 7)).toEqual(new Maybe.Nothing)
+      expect(eat(dog, catFood, 3)).toEqual(new Maybe.Nothing)
+      expect(eat("hello world", catFood, 3)).toEqual(new Maybe.Nothing)
+    })
+  })
+
+  describe("a function with receiver", function() {
+    cat.name = "kitty"
+
+    cat.eat = match(
+      pattern(CatFood, Number,
+        function(food, amount) {
+          return this.name + " has ate " + amount + " of cat food"
+        }),
+
+      pattern(_, _,
+        function(_, _) { return this.name + " can't eat that :(" })
+    )
+
+    it("calls functions correctly on receiver", function() {
+      expect(cat.eat(catFood, 3)).toEqual("kitty has ate 3 of cat food")
+      expect(cat.eat(catFood)()()()(3)).toEqual("kitty has ate 3 of cat food")
+      expect(cat.eat(dogFood, 3)).toEqual("kitty can't eat that :(")
+    })
+  })
+
+  describe("factorial example", function() {
+    var factorial = match(
+      pattern(0,
+        function(_) { return 1 }),
+
+      pattern(Number,
+        function(n) { return n * factorial(n - 1) })
+    )
+
+    it("works with raw values in pattern correctly", function() {
+      expect(factorial(0)).toEqual(1)
+      expect(factorial(5)).toEqual(120)
     })
   })
 })
--- src/monad.js
+++ src/monad.js
@@ -34,6 +34,88 @@ M.CurriedFunction.prototype.apply = function(target, args) {
   }, this.length - args.length)
 }
 
+// Pattern Matching
+M._ = function() {}
+
+M.match = function() {
+  var patterns = [].slice.call(arguments)
+  return M.enableMatch(patterns)
+}
+
+M.pattern = function() {
+  var args = [].slice.call(arguments),
+      fn = args.pop()
+  return new M.Pattern(args, fn)
+}
+
+M.ofType = function(type, value) {
+  if (type === M._) {
+    return true
+  }
+
+  if (type === Number) {
+    return typeof value === "number"
+  }
+
+  if (type === String) {
+    return typeof value === "string"
+  }
+
+  if (type === Boolean) {
+    return typeof value === "boolean"
+  }
+
+  if (type instanceof Function) {
+    return value instanceof type
+  }
+
+  return value === type
+}
+
+M.enableMatch = function(patterns) {
+  var fn = function() {
+    var args = [].slice.call(arguments),
+        foundPattern = undefined
+
+    patterns.map(function(pattern) {
+      if (pattern.matches(args)) {
+        foundPattern = foundPattern || pattern
+      }
+    })
+
+    if (!foundPattern) {
+      throw "failed pattern matching, consider using catch-all patterns"
+    }
+
+    return foundPattern.apply(this, args)
+  }
+
+  return M.enableCurriedFunction(fn, patterns[0].length)
+}
+
+M.Pattern = function(args, fn) {
+  this.length = args.length
+  this.args = args
+  this.fn = fn
+}
+
+M.Pattern.prototype.matches = function(args) {
+  var ok = true,
+      that = this
+
+  args.map(function(arg, index) {
+    if (!M.ofType(that.args[index], arg)) {
+      ok = false
+    }
+  })
+
+  return ok
+}
+
+M.Pattern.prototype.apply = function(target, args) {
+  return this.fn.apply(target, args)
+}
+
 // Functors
 M.functor = function(type, opts) {
   type.prototype.fmap = M.curry(opts.fmap)
